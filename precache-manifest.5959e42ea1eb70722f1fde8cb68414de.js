d22fc4f0c98aeff9d870651b8ee5366634aa158c
JeffShell <875899952@qq.com>
2020-12-05 21:39:33 +0800

    deploy

---
+++ precache-manifest.5959e42ea1eb70722f1fde8cb68414de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d8d7724b2b1cde4c33",
+    "url": "/morney-demo-website/css/app.68c39454.css"
+  },
+  {
+    "revision": "49c5a236222fa2105ce811cc6b84d88d",
+    "url": "/morney-demo-website/index.html"
+  },
+  {
+    "revision": "07d8d7724b2b1cde4c33",
+    "url": "/morney-demo-website/js/app.bbd75930.js"
+  },
+  {
+    "revision": "f8531c0199d8b3c15a51",
+    "url": "/morney-demo-website/js/chunk-vendors.5180712d.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-demo-website/manifest.json"
+  },
+  {
+    "revision": "0c6a8ffefe6b4c47848ab37f53dec6bb",
+    "url": "/morney-demo-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-demo-website/robots.txt"
+  }
+]);
